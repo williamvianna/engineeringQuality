1091ea41a50a3de9dc41b67334441f837ff02de1
William <viannawp@gmail.com>
2025-10-17 12:40:16 -0300

    Atualização

--- k6/script.js
+++ k6/script.js
@@ -1,6 +1,1 @@
-// k6/script.js
-import http from 'k6/http';
-
-export default function () {
-  http.get('https://test.k6.io');
-}
+import http from 'k6/http'; export default function () { http.get('https://test.k6.io'); }
