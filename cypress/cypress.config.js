d1a5fc396ab30327c6feffc81dc7a48dc48422fe
William <viannawp@gmail.com>
2025-10-25 11:05:14 -0300

    Atualizado o workflow.

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -4,31 +4,24 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: "https://automationpratice.com.br/",
     defaultCommandTimeout: 5000,
-    // viewportWidth: 1920,
-    // viewportHeight: 1080,
-    // Configura listeners e plugins
     setupNodeEvents(on, config) {
-      // Integrando o cypress-mochawesome-reporter
       require('cypress-mochawesome-reporter/plugin')(on);
     },
 
-    // Relatórios
     reporter: 'cypress-mochawesome-reporter',
     reporterOptions: {
-      reportDir: 'cypress/mochawesome-report',  // pasta dos relatórios
+      reportDir: 'cypress/mochawesome-report',
       charts: true,
       reportPageTitle: 'Relatório Cypress',
-      embeddedScreenshots: true,               // screenshots dentro do relatório
+      embeddedScreenshots: true,
       overwrite: false,
       html: true,
       json: true
     },
 
-    // Screenshots
     screenshotOnRunFailure: true,
     screenshotsFolder: 'cypress/screenshots',
 
-    // Vídeos
     video: true,
     videosFolder: 'cypress/videos'
   },
