ecee53b6c28c2a237dd685919f8eb889bb648e82
William <viannawp@gmail.com>
2026-04-09 19:53:14 -0300

    feat(java): Estrutura condicional

---
+++ java/estruturaCondicional/Condicional.java
@@ -0,0 +1,51 @@
+// import java.util.Scanner;
+
+// public class Condicional {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+        // int x = 5;
+        // int hora;
+        // Estrutura de condinção simples (verdadeira)
+        // System.out.println("Bom dia!");
+        // if (x > 0) {
+        //     System.out.println("Boa tarde!");
+        // }
+
+        // System.out.println("Boa noite!");
+
+        // Estrutura de condinção simples (falsa)
+    //   System.out.println("Bom dia!");
+    //     if (x < 0) {
+    //         System.out.println("Boa tarde!");
+    //     }
+
+    //     System.out.println("Boa noite!");
+
+    // Estrutura composta
+        // System.out.println("Que horas são?");
+        // hora = sc.nextInt();
+
+        // if (hora < 12) {
+        //     System.out.println("Bom dia!");
+        // }
+        // else {
+        //     System.out.println("Boa tarde!");
+        // }
+
+        // IF ELSE
+//         System.out.println("Que horas são?");
+//         hora = sc.nextInt();
+
+//         if (hora < 12) {
+//             System.out.println("Bom dia!");
+//         }
+//         else if (hora < 18) {
+//             System.out.println("Boa tarde!");
+//         }
+//         else {
+//             System.out.println("Boa noite!");
+//         }
+
+//         sc.close();
+//     }
+// }
