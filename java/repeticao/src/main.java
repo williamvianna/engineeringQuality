90f49468e600b6f1df5a9bbbed45eaf2b30e4796
William <viannawp@gmail.com>
2026-05-05 20:56:01 -0300

    feat(java): Exercício de For.

--- java/repeticao/src/main.java
+++ java/repeticao/src/main.java
@@ -4,17 +4,33 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
+        // While
+        // System.out.println("Digite um número:");
+
+        // Scanner sc = new Scanner(System.in);
+        // int x = sc.nextInt();   
+
+        // int soma = 0;
+        // while (x != 0) {
+        //     soma = soma + x;
+        //     x = sc.nextInt();            
+        // }
+        // System.out.println("A soma dos números é: " + soma);
+
+        // sc.close();
+
+        // For
         System.out.println("Digite um número:");
 
         Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();   
+        int N = sc.nextInt();   
 
         int soma = 0;
-        while (x != 0) {
-            soma = soma + x;
-            x = sc.nextInt();            
+        for (int i=0; i<N; i++) {
+            int x = sc.nextInt();
+            soma = soma + x;                
         }
-        System.out.println("A soma dos números é: " + soma);
+        System.out.println("A soma dos números, até N é: " + soma);
 
         sc.close();
     }
