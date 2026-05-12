59db90ec6cfd13b618c18b3b5fae8c5f476ffd23
William <viannawp@gmail.com>
2026-05-12 19:44:27 -0300

    feat(java): Exercício Do-While.

--- java/repeticao/src/main.java
+++ java/repeticao/src/main.java
@@ -1,5 +1,6 @@
 package repeticao.src;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class main {
@@ -19,18 +20,38 @@ public class main {
 
         // sc.close();
 
-        // For
-        System.out.println("Digite um número:");
 
+    //     // For
+    //     System.out.println("Digite um número:");
+
+    //     Scanner sc = new Scanner(System.in);
+    //     int N = sc.nextInt();   
+
+    //     int soma = 0;
+    //     for (int i=0; i<N; i++) {
+    //         int x = sc.nextInt();
+    //         soma = soma + x;                
+    //     }
+    //     System.out.println("A soma dos números, até N é: " + soma);
+
+    //     sc.close();
+    // }
+
+
+        // Do-While
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();   
 
-        int soma = 0;
-        for (int i=0; i<N; i++) {
-            int x = sc.nextInt();
-            soma = soma + x;                
-        }
-        System.out.println("A soma dos números, até N é: " + soma);
+        char resp;
+
+        do {
+            System.out.print("Digite a temperatura em Celsius: ");
+            double C = sc.nextDouble();
+            double F = 9.0 * C / 5.0 + 32.0;
+            System.out.printf("Equivalente em Fahrenheit: %.1f%n", F);
+            System.out.print("Deseja repetir (s/n)? ");
+            resp = sc.next().charAt(0);
+        } while (resp != 'n');
 
         sc.close();
     }
