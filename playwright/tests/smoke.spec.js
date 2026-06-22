9d07a2ef2c9bf55b352b3220934107bdde19a937
William <viannawp@gmail.com>
2026-06-22 13:55:49 -0300

    feat(playwright): remove pasta antiga tests e define e2e como diretorio padrão

--- playwright/tests/smoke.spec.js
+++
@@ -1,8 +0,0 @@
-// playwright/tests/smoke.spec.js
-const { test, expect } = require('@playwright/test');
-
-test('Smoke Test: Google homepage loads', async ({ page }) => {
-  await page.goto('https://www.google.com');
-  // Espera que o título contenha 'Google'
-  await expect(page).toHaveTitle(/Google/);
-});
