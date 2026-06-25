194222b6d373978258ed4cfd8dabdb816b9253fe
William <viannawp@gmail.com>
2026-06-25 18:38:02 -0300

    ci: adicionar suporte a secrets e injetar credenciais no workflow

--- playwright/CredenciaisHelper.js
+++ playwright/CredenciaisHelper.js
@@ -1,11 +1,33 @@
-import credentials from './credentials.json';
+import fs from 'fs';
+import path from 'path';
 
 export class CredenciaisHelper {
   static getUsername() {
-    return credentials.username;
+    // Se existir a variável no ambiente (GitHub), usa ela. Se não, tenta ler o arquivo local.
+    if (process.env.PLAYWRIGHT_USER) {
+      return process.env.PLAYWRIGHT_USER;
+    }
+    
+    const credentialsPath = path.resolve(__dirname, './credentials.json');
+    if (fs.existsSync(credentialsPath)) {
+      const credentials = JSON.parse(fs.readFileSync(credentialsPath, 'utf-8'));
+      return credentials.username;
+    }
+    
+    throw new Error("Credenciais de usuário não encontradas localmente ou no ambiente.");
   }
 
   static getPassword() {
-    return credentials.password;
+    if (process.env.PLAYWRIGHT_PASSWORD) {
+      return process.env.PLAYWRIGHT_PASSWORD;
+    }
+
+    const credentialsPath = path.resolve(__dirname, './credentials.json');
+    if (fs.existsSync(credentialsPath)) {
+      const credentials = JSON.parse(fs.readFileSync(credentialsPath, 'utf-8'));
+      return credentials.password;
+    }
+
+    throw new Error("Credenciais de senha não encontradas localmente ou no ambiente.");
   }
 }
