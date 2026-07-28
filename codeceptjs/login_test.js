c628d7bb7ca7d4952808ae240d0464a79bc074ca
William <viannawp@gmail.com>
2026-07-28 19:21:51 -0300

    feat(codeceptjs): criado os cenários de testes do Login.

--- codeceptjs/login_test.js
+++ codeceptjs/login_test.js
@@ -2,16 +2,36 @@ Feature("login");
 
 Scenario("Login com sucesso", ({ I }) => {
   I.amOnPage("http://automationpratice.com.br");
+  I.click("a[href='/login']");
+  I.waitForText("Login", 5);
+  I.fillField("#user", "teste@gmail.com");
+  I.fillField("#password", "123456");
+  I.click("#btnLogin");
+  I.waitForText("Login realizado", 3);
 });
 
 Scenario("Tentando Logar digitando apenas e-mail", ({ I }) => {
   I.amOnPage("http://automationpratice.com.br");
+  I.click("a[href='/login']");
+  I.waitForText("Login", 5);
+  I.fillField("#user", "teste@gmail.com");
+  I.click("#btnLogin");
+  I.waitForText("Senha inválida.", 3);
 });
 
 Scenario("Tentando Logar sem digitar e-mail e senha", ({ I }) => {
   I.amOnPage("http://automationpratice.com.br");
+  I.click("a[href='/login']");
+  I.waitForText("Login", 5);
+  I.click("#btnLogin");
+  I.waitForText("E-mail inválido.", 3);
 });
 
 Scenario("Tentando Logar digitando apenas senha", ({ I }) => {
   I.amOnPage("http://automationpratice.com.br");
+  I.click("a[href='/login']");
+  I.waitForText("Login", 5);
+  I.fillField("#password", "123456");
+  I.click("#btnLogin");
+  I.waitForText("E-mail inválido.", 3);
 });
